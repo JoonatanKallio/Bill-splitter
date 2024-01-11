e0b413693b71da8332a3872634fb8a33a29ff6ee
Joonatan Kallio <kallio.joonatan1@gmail.com>
2024-01-11 17:44:06 +0200

    Finished monitor view

--- src/components/TipCalculator.jsx
+++ src/components/TipCalculator.jsx
@@ -1,9 +1,33 @@
+import {useEffect, useState} from "react";
 import logo from "../assets/logo.svg";
-import TipButton from "./TipButton";
 
 export default function TipCalculator() {
+    const [tip, setTip] = useState("");
+    const [bill, setBill] = useState("0");
+    const [people, setPeople] = useState("");
+    const [customTip, setCustomTip] = useState("");
+    const [tipAmount, setTipAmount] = useState("0.00");
+    const [total, setTotal] = useState("0.00");
     function reset() {
-        console.log("reset");
+        setTip("");
+        setBill("");
+        setPeople("");
+        setCustomTip("");
+        setTipAmount("0");
+        setTotal("0");
+    }
+    useEffect(() => {
+        if (bill && customTip && people) {
+            console.log(customTip)
+            const tipPerPerson = (bill * (customTip / 100)) / people;
+            setTipAmount(tipPerPerson);
+            const totalPerPerson = tipPerPerson + (bill / people);
+            setTotal(totalPerPerson);
+        }
+    }, [customTip, people, bill])
+    function customTipOnChange(value) {
+        setCustomTip(value);
+        setTip("");
     }
     return (
         <div className="main">
@@ -18,25 +42,22 @@ export default function TipCalculator() {
                                 <div className="billText">
                                     Bill
                                 </div>
-                                <input className="billAmount" placeholder="0" />
+                                <input className="billAmount" value={bill} placeholder="0" onChange={(e) => setBill(e.target.value)} />
                             </div>
                             <div className="selectTip">
                                 <div className="text">
                                     Select tip %
                                 </div>
                                 <div className="tipGrid">
-                                    <TipButton tipAmount="5" />
-                                    <TipButton tipAmount="10" />
-                                    <TipButton tipAmount="15" />
-                                    <TipButton tipAmount="25" />
-                                    <TipButton tipAmount="50" />
-                                    <input placeholder="Custom" className="customTip" />
+                                    <TipButton tipAmount={5} tip={tip} setTip={setTip} setCustomTip={setCustomTip} />
+                                    <TipButton tipAmount={10} tip={tip} setTip={setTip} setCustomTip={setCustomTip} />
+                                    <TipButton tipAmount={15} tip={tip} setTip={setTip} setCustomTip={setCustomTip} />
+                                    <TipButton tipAmount={25} tip={tip} setTip={setTip} setCustomTip={setCustomTip} />
+                                    <TipButton tipAmount={50} tip={tip} setTip={setTip} setCustomTip={setCustomTip} />
+                                    <input min="0" placeholder="Custom" value={customTip} className="customTip" onChange={(e) => customTipOnChange(e.target.value)} />
                                 </div>
                             </div>
-                            <div className="numOfPpl">
-                                <div>Number of People</div>
-                                <input placeholder="0" />
-                            </div>
+                            <PeopleInput people={people} setPeople={setPeople} bill={bill} tip={tip} setTipAmount={setTipAmount} setTotal={setTotal} />
                         </div>
 
                     </div>
@@ -45,22 +66,82 @@ export default function TipCalculator() {
                             <div className="tipAmountPerPerson">
                                 <div className="text">
                                     <div>Tip Amount</div>
-                                    <div>/ person</div>
+                                    <div className="person">/ person</div>
                                 </div>
-                                <div className="amount">$0.00</div>
+                                <div className="amount">
+                                    $
+                                    {Math.round(tipAmount * 100) / 100}
+                                </div>
                             </div>
                             <div className="tipAmountPerPerson">
                                 <div className="text">
                                     <div>Total</div>
-                                    <div>/ person</div>
+                                    <div className="person">/ person</div>
                                 </div>
-                                <div className="amount">$0.00</div>
+                                <div className="amount">
+                                    $
+                                    {Math.round(total * 100) / 100}
+                                </div>
                             </div>
                             <button type="button" className="reset" onClick={reset}>RESET</button>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+    );
+}
+
+function TipButton({
+    tipAmount, tip, setTip, setCustomTip,
+}) {
+    function tipClicked() {
+        setTip(tipAmount);
+        setCustomTip("");
+    }
+    if (tip === tipAmount) {
+        return (
+            <button type="button" className="tipButton checked" onClick={tipClicked}>
+                {tipAmount}
+                %
+            </button>
+        );
+    }
+    return (
+        <button type="button" className="tipButton" onClick={tipClicked}>
+            {tipAmount}
+            %
+        </button>
+    );
+}
+
+function PeopleInput({
+    people, setPeople, bill, tip, setTipAmount, setTotal,
+}) {
+    if (people === "0") {
+        return (
+            <div className="numOfPeople">
+                <div className="peopleText">
+                    <div>Number of People</div>
+                    <div className="peopleErrorText">Can&apos;t be zero</div>
+                </div>
+                <input value={people} placeholder="0" className="peopleError" onChange={(e) => setPeople(e.target.value)} />
+            </div>
+        );
+    }
+    if (bill && tip && people) {
+        const tipPerPerson = (bill * (tip / 100)) / people;
+        setTipAmount(tipPerPerson);
+        const totalPerPerson = tipPerPerson + (bill / people);
+        setTotal(totalPerPerson);
+    }
+    return (
+        <div className="numOfPeople">
+            <div className="peopleText">
+                <div>Number of People</div>
+                <div className="peopleErrorText" style={{ display: "none" }}>Can&apos;t be zero</div>
+            </div>
+            <input value={people} placeholder="0" onChange={(e) => setPeople(e.target.value)} />
         </div>
     );
 }
